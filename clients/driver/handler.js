0a9eeef371f44b8f5afaa63665905ee4d67b31a9
rpruazol <rpruazol@gmail.com>
2023-08-13 16:38:06 -0400

    queue management in server.js

--- clients/driver/handler.js
+++ clients/driver/handler.js
@@ -1,13 +1,17 @@
 "use strict";
 
+const {pickup, inTransit, delivered, received} = require('../../eventPool.js')
+
+
 function pickupConfirmation(payload, socket) {
   console.log(`picking up ${payload.orderId}`);
-  socket.emit("in-transit", payload);
+  socket.emit(received, payload)
+  socket.emit(inTransit, payload);
 }
 
 function deliveredConfirmation(payload, socket) {
   console.log(`delivered ${payload.orderId}`);
-  socket.emit("delivered", payload);
+  socket.emit(delivered, payload);
 }
 
 module.exports = { pickupConfirmation, deliveredConfirmation };
--- clients/driver/index.js
+++ clients/driver/index.js
@@ -1,14 +1,23 @@
 'use strict'
 
 const {pickupConfirmation, deliveredConfirmation} = require('./handler')
-const {pickup, inTransit, delivered} = require('../../eventPool.js')
+const {pickup, inTransit, delivered, received} = require('../../eventPool.js')
 const {io} = require('socket.io-client');
-const client = io('ws://localhost:3000/caps');
+const client = io(
+  'ws://localhost:3000/caps',
+  {
+    auth: {
+      role: 'driver'
+    }
+  }
+  );
 
 
 client.on(pickup, (payload) => {
   pickupConfirmation(payload, client)
   setTimeout(() => {
     deliveredConfirmation(payload, client)}
-    , 5000);
+    , 1000);
 });
+
+
